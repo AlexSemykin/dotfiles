20f5ed4483f26fa160e7465f9c7d4dbd8a484cb6
AlexSemykin <alex.semykin21@gmail.com>
2022-12-19 14:45:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,33 +9,32 @@ module.exports = {
     'vim --with-client-server --with-override-system-vi',
     'lsd',              //https://github.com/Peltoche/lsd
     'autojump',         //https://github.com/wting/autojump
-    'sourcetree',
     'yarn',
-    'asciinema'
+    'asciinema',
     'lssh',
     'hub',
     'tree',
-    'hadolint'   //https://github.com/hadolint/hadolint
+    'hadolint',         //https://github.com/hadolint/hadolint
     'gnebbia/kb/kb',
     'dust',
-    'bit' //https://github.com/chriswalz/bit
+    'bit',              //https://github.com/chriswalz/bit
     'youtube-dl',
+    'docker',
   ],
   cask: [
-    'docker',
-    'iterm2',
-    'visual-studio-code',
-    'slack',
-    'clipy',
-    'alfred'
-    'android-studio',
-    'android-platform-tools',
-    'brave-browser',
-    'discord',
     'firefox',
     'brave-browser',
     'google-chrome',
+    'iterm2',
+    'sourcetree',
+    'visual-studio-code',
+    'slack',
+    'clipy',
+    'alfred',
+    'android-studio',
+    'android-platform-tools',
     'intellij-idea',
+    'discord',
     'mounty',
     'postman',
     'telegram',
